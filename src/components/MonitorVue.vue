3bc9282e6e494c692afcabba8e26fbafff9cdd01
Mihray <2636853047@qq.com>
2022-08-23 16:23:48 +0800

    Create MonitorVue.vue

---
+++ src/components/MonitorVue.vue
@@ -0,0 +1,29 @@
+<template>
+    <div id="demo">{{fullName}}</div>
+</template>
+
+<script>
+export default {
+    el:'#demo',
+    data(){
+        return{
+            firstName:'Foo',
+            lastName:'Bar',
+            // fullName:'Foo Bar'
+        }
+    },
+    // Watch:{
+    //     firstName:function(val){
+    //         this.fullName=val+''+this.lastName
+    //     },
+    //     lastName:function(val){
+    //         this.fullName=this.firstName+''+val
+    //     }
+    // }
+    computed:{
+        fullName:function(){
+            return this.firstName+' '+this.lastName
+        }
+    }
+}
+</script>
