c13bc74dc32ec8e003ba9c7a9749629c5e42aa4f
Mihray <2636853047@qq.com>
2022-08-23 13:08:18 +0800

    Create DlrowOlloeh.vue

---
+++ src/components/DlrowOlloeh.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="example">
+        <p>Original message:"{{message}}"</p>
+        <p>Computed reversed message:"{{reversedMessage}}"</p>
+    </div>
+</template>
+
+<script>
+export default {
+    el:'#example',
+   
+        data(){
+           return{
+              message:'hello',
+           }
+        },
+        computed:{
+            reversedMessage:function(){
+                return this.message.split('').reverse().join('')
+            }
+        }
+    }
+</script>
