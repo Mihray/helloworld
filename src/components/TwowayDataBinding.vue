feb4246cafe62b80d0ad8debf7f639952760cb3d
Mihray <2636853047@qq.com>
2022-08-24 21:07:16 +0800

    Create TwowayDataBinding.vue

---
+++ src/components/TwowayDataBinding.vue
@@ -0,0 +1,31 @@
+<template>
+    <div id="select">
+        <select v-model="selected">
+            <option disabled value="">请选择</option>
+            <option>A</option>
+            <option>B</option>
+            <option>C</option>
+        </select>
+        <span>Selected:{{selected}}</span>
+    </div>
+    <div id="select2">
+        <select v-model="selected2" multiple style="width:100px">
+            <option disabled value="">请选择</option>
+            <option>A</option>
+            <option>B</option>
+            <option>C</option>
+        </select>
+        <span>Selected:{{selected2}}</span>
+    </div>
+</template>
+<script>
+export default {
+    el:'select',
+    data(){
+        return {
+            selected:'',
+            selected2:[]
+        }
+    }
+}
+</script>
