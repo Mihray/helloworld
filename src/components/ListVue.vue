b34f78c6b7e6289bef0fe240e3caf24b29b7debf
Mihray <2636853047@qq.com>
2022-08-24 15:39:22 +0800

    Create ListVue.vue

---
+++ src/components/ListVue.vue
@@ -0,0 +1,31 @@
+<template>
+    <ul id="example-1">
+        <li v-for="item in items" :key="item.message">
+        {{item.message}}
+        </li>
+    </ul>
+    <ul v-for="set in sets">
+        <li v-for="n in even(set)"></li>
+    </ul>
+</template>
+<script>
+export default {
+    el:'#example-1',
+    data(){
+        return{
+            items:[
+                {message:'Foo'},
+                {message:'Bar'},
+            ],
+            sets:[[1,2,4,5],[8,6,9]]
+        }
+    },
+    methods:{
+        even:function(numbers){
+            return numbers.filter(function(number){
+                return number%2===0
+            })
+        }
+    }
+}
+</script>
